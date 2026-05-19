4891ccf0a5517bc28c2f7c0408bad159f5e12e7a
ABDUL MALIK <malik.kasganj@gmail.com>
2026-05-20 01:43:18 +0530

    Character to ASCII

---
+++ ASCII.java
@@ -0,0 +1,7 @@
+public class ASCII{
+    public void ascii(Character ch){
+        
+        int ASCII_VALUE=(int) ch;
+        System.out.println("THE ASCII value of " + ch + " is: " + ASCII_VALUE);
+    }
+}
